599716bc1c7182e56e2ff637e9e3df391ab969e1
Amit Sawhney <amit.sawhney2002@gmail.com>
2021-04-03 19:27:47 -0500

    Add Pixel enumeration documentation

--- include/enums/pixel.h
+++ include/enums/pixel.h
@@ -1,9 +1,13 @@
 
 namespace naivebayes {
+
+/**
+ * Represents a singular the status of a singular Pixel in an image
+ */
 enum class Pixel {
   kUnshaded,
   kPartiallyShaded,
   kShaded,
   kNumShades = 3,
 };
-}
+} // namespace naivebayes
